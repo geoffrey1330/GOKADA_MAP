f46e991900112b4cca28a0e71d5448b7bff0f891
geoffrey1330 <israelgeoffrey13@gmail.com>
2021-04-11 09:11:41 +0100

    second commit

--- src/Components/Marker.jsx
+++ src/Components/Marker.jsx
@@ -6,11 +6,13 @@ const Wrapper = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  width: 18px;
-  height: 18px;
-  background-color: #000;
-  border: 2px solid #fff;
-  border-radius: 100%;
+  width: 38px;
+  height: 38px;
+  
+  background-image: url("static/media/location-pin.7d241082.svg");
+  background-repeat:no-repeat;
+  
+  
   user-select: none;
   transform: translate(-50%, -50%);
   cursor: ${(props) => (props.onClick ? "pointer" : "default")};
